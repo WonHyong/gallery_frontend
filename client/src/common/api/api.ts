495f6d16302e6b3f8022483f83e033a4ca446d88
minseok <tir2986@naver.com>
2024-02-26 22:59:16 +0900

    fix: env importing

--- client/src/common/api/api.ts
+++ client/src/common/api/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_API_URL = `${import.meta.env.BASE_URL}/api`;
+const BASE_API_URL = `${import.meta.env.VITE_BASE_URL}/api`;
 // const BASE_API_URL = 'http://localhost/api';
 
 const api = axios.create({
